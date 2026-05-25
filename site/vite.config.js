d09a34b4b02a84fece98aabe090c969c4ddd2f24
Péter Ónadi <onadipeter.nyirsuli@gmail.com>
2026-05-25 22:12:18 +0200

    fix config after DNS change

--- site/vite.config.js
+++ site/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: '/Post-mortems-Analysis/',
   plugins: [react()],
 })
